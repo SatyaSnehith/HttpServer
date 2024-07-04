030397cf08706985ff6ee84b1f70109ee560cf92
SatyaSnehith <nsssnehit76@gmail.com>
2024-07-05 01:12:27 +0530

    update Json

--- src/main/kotlin/com/satyasnehith/net/sharer/client/SharerClient.kt
+++ src/main/kotlin/com/satyasnehith/net/sharer/client/SharerClient.kt
@@ -7,28 +7,47 @@ import com.satyasnehith.net.sharer.message.ConnectResponse
 import com.satyasnehith.net.util.readLine
 import com.satyasnehith.net.util.writeCrlf
 import kotlinx.serialization.encodeToString
+import java.io.InputStream
+import java.io.OutputStream
 import java.net.Socket
 
-class SharerClient: Service() {
-    var socket: Socket? = null
+class SharerClient(
+    private val host: String = "localhost",
+    private val port: Int = 1111
+): Service() {
+    private lateinit var socket: Socket
+    private lateinit var outputStream: OutputStream
+    private lateinit var inputStream: InputStream
+    private lateinit var password: String
+
+    fun start(
+        password: String
+    ) {
+        this.password = password
+        start()
+    }
 
     override fun start() {
         super.start()
-        socket = Socket("localhost",1111)
-        val outputStream = socket!!.getOutputStream()
-        val inputStream = socket!!.getInputStream()
-        val connectRequest = ConnectRequest()
+        socket = Socket(host, port)
+        outputStream = socket.getOutputStream()
+        inputStream = socket.getInputStream()
+
+        processConnectionRequest()
+    }
+
+    private fun processConnectionRequest() {
+        val connectRequest = ConnectRequest(password)
         val connectRequestJson = json.encodeToString(connectRequest)
         outputStream.writeCrlf(connectRequestJson)
         println("SENT: ConnectRequest -> $connectRequestJson")
         val connectResponseJson = inputStream.readLine()
         val connectResponse = json.decodeFromString<ConnectResponse>(connectResponseJson)
         println("RECEIVED: ConnectResponse -> $connectResponseJson")
-
     }
 
     override fun stop() {
-        socket?.close()
+        socket.close()
         super.stop()
     }
 
